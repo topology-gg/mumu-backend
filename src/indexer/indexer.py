3c3a623cdb26dcc9ea3bd6a8f6af9c492f94fcbb
Gregory Edison <gregory.edison1993@gmail.com>
2022-12-23 16:09:40 +0100

    feat: indexer update

--- src/indexer/indexer.py
+++ src/indexer/indexer.py
@@ -4,11 +4,13 @@ from starknet_py.contract import FunctionCallSerializer, identifier_manager_from
 from .abis import mech_state_abi, grid_abi, new_simulation_abi, end_summary_abi
 from .types import NewSimulation, EndSummary
 
-indexer_id = "mumu_indexer_s2_1"
+indexer_id = "mumu_indexer_s2_daw_1"
 
 new_simulation_decoder = FunctionCallSerializer(
     abi=new_simulation_abi,
-    identifier_manager=identifier_manager_from_abi([mech_state_abi, grid_abi, new_simulation_abi]),
+    identifier_manager=identifier_manager_from_abi(
+        [mech_state_abi, grid_abi, new_simulation_abi]
+    ),
 )
 
 end_summary_decoder = FunctionCallSerializer(
@@ -16,21 +18,25 @@ end_summary_decoder = FunctionCallSerializer(
     identifier_manager=identifier_manager_from_abi([end_summary_abi]),
 )
 
+
 def decode(event_ns, event_es):
     ns = NewSimulation.from_iter(iter(event_ns.data))
     es = EndSummary.from_iter(iter(event_es.data))
     return dict(
-        solver = ns.solver,
-        mechs = [m.to_json() for m in ns.mechs],
-        instructions_sets = ns.instructions_sets,
-        instructions = ns.instructions,
-        operators_inputs = [i.to_json() for i in ns.operators_inputs],
-        operators_outputs = [i.to_json() for i in ns.operators_outputs],
-        operators_type = ns.operators_type,
-        static_cost = ns.static_cost,
-        delivered = es.delivered,
-        latency = es.latency,
-        dynamic_cost = es.dynamic_cost,
+        solver=ns.solver,
+        music_title=ns.music_title,
+        mechs=[m.to_json() for m in ns.mechs],
+        instructions_sets=ns.instructions_sets,
+        instructions=ns.instructions,
+        operators_inputs=[i.to_json() for i in ns.operators_inputs],
+        operators_outputs=[i.to_json() for i in ns.operators_outputs],
+        operators_type=ns.operators_type,
+        faucets=[i.to_json() for i in ns.faucets],
+        sinks=[i.to_json() for i in ns.sinks],
+        static_cost=ns.static_cost,
+        delivered=es.delivered,
+        latency=es.latency,
+        dynamic_cost=es.dynamic_cost,
     )
 
 
@@ -40,13 +46,10 @@ async def handle_events(info: Info, block_events: NewEvents):
     new_simulations = block_events.events[0::2]
     end_summaries = block_events.events[1::2]
 
-    events = [
-        decode(e1, e2)
-        for (e1, e2) in zip(new_simulations, end_summaries)
-    ]
+    events = [decode(e1, e2) for (e1, e2) in zip(new_simulations, end_summaries)]
 
     # Insert multiple documents in one call.
-    await info.storage.insert_many("mumu-s2-events", events)
+    await info.storage.insert_many("mumu-s2-daw-events", events)
 
 
 async def run_indexer(server_url=None, mongo_url=None, restart=None):
@@ -68,7 +71,9 @@ async def run_indexer(server_url=None, mongo_url=None, restart=None):
     #
     # For now, this also helps the SDK map between human-readable
     # event names and StarkNet events.
-    contract_address = "0x021317adbb27474f10485c4b866b3d9c94519a455da5b2bf1fe6e35832d901ea"
+    contract_address = (
+        "0x04a80d0a8ce30aa76103693f41aeb265d2cb7bd164c52f262b571f42aa572d04"
+    )
     runner.add_event_filters(
         filters=[
             EventFilter.from_event_name(
@@ -78,9 +83,9 @@ async def run_indexer(server_url=None, mongo_url=None, restart=None):
             EventFilter.from_event_name(
                 name="end_summary",
                 address=contract_address,
-            )
+            ),
         ],
-        index_from_block=464_138,
+        index_from_block=541_881,
     )
 
     print("Initialization completed. Entering main loop.")
